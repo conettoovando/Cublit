08187551488842605133df6c3eef88b04bfafeda
conettoovando <cristian.onetto@hotmail.com>
2023-03-06 22:31:06 -0300

    restore

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-
 import Login from "./screens/Login";
 import Signup from "./screens/Signup";
 import Home from "./screens/Home";
@@ -10,7 +9,7 @@ const Stack = createStackNavigator();
 
 function Navigations (){
   return (
-    <Stack.Navigator defaultScreenOptions={Login} >
+    <Stack.Navigator initialRouteName={Login} >
       <Stack.Screen name ="Login" component={Login}/>
       <Stack.Screen name ="Signup" component={Signup}/>
       <Stack.Screen name ="Home" component={Home}/>
